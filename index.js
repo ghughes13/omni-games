1e3747d91e852491cdfc77e90e29f7fdcd452d07
divyanshdahiya <dahiya.divyansh1@gmail.com>
2021-07-31 19:14:45 +0530

    hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Hi, I'm omni bot!`);
+});
+
+
+client.login('ODcxMDExNjAwNDc0MjA2MjQ4.YQVGxA.A3K7oRVDNcB85SKkDO1kmvonpZE');
