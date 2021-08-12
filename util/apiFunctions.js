b93d049e5493953cd7c8d8d939ef6093d15cac6f
ghughes13 <battletank130@gmail.com>
2021-08-12 11:29:32 -0500

    added findUser funciton

--- util/apiFunctions.js
+++ util/apiFunctions.js
@@ -4,25 +4,25 @@ import mongoose from "mongoose";
 dotenv.config();
 
 mongoose
-  .connect(
-    `mongodb+srv://OmniBotBuilder:${process.env.DBPASS}${process.env.DBUSER}.kx2vg.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected to DB");
+  })
   .catch(() => console.error("Unable to connect to DB"));
 
 mongoose.connection.on("connected", () => {});
 
 const Schema = mongoose.Schema;
 
-const omniGamesSchema = new Schema({
-  discordId: Number,
-  steamId: Number,
+const omniGamesSchemas = new Schema({
+  discordId: String,
+  steamId: String,
 });
 
-const omniGamesModel = mongoose.model("omniGamesSchema", omniGamesSchema);
+const omniGamesModel = mongoose.model("omniGamesSchemas", omniGamesSchemas);
 
 const createNewUser = (discordId, steamId) => {
   const newUserMap = new omniGamesModel({
@@ -37,4 +37,14 @@ const createNewUser = (discordId, steamId) => {
   });
 };
 
-export { createNewUser };
+const findUser = (discordId) => {
+  omniGamesModel
+    .find({ discordId: discordId }) //discordId type should === String
+    .exec()
+    .then((data) => data)
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export { createNewUser, findUser };
